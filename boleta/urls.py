57aef62d8b9484feb2fc2ee81683a7021d8b326d
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-05 13:37:54 -0400

    Update urls.py

--- boleta/urls.py
+++ boleta/urls.py
@@ -13,5 +13,5 @@ router.register("Cliente",views.ClienteApi, basename="cliente")
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('cliente/<int:pk>/', cliente_detail_view, name= "cliente_api"),
+    path('cl/<int:pk>/', cliente_detail_view, name= "cliente_api"),
 ]
