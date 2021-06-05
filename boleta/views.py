65a17e54ea4ab88e951d47e86bb03c3ab06298c5
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-05 13:27:22 -0400

    Update views.py

--- boleta/views.py
+++ boleta/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Boleta, Cliente, Producto
 from .serializers import BoletaSerializer, ProductoSerializer, ClienteSerializer
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
