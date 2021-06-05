1052524e5e4377b7d2b326d89ef6e9804ff0e775
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-05 13:31:12 -0400

    Update urls.py

--- boleta/urls.py
+++ boleta/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from . import views
+from boleta.views import (
+	cliente_detail_view,
+	)
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
