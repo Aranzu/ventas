24aad92ea5171935f1f8d04fe69eb3ee8a053342
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-07 20:30:57 -0400

    Update urls.py

--- boleta/urls.py
+++ boleta/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from . import views
 from boleta.views import (
-	cliente_detail_view,
+	cliente_detail_view, boleta_detail_view
 	)
 from rest_framework.routers import DefaultRouter
 
@@ -14,4 +14,5 @@ router.register("Cliente",views.ClienteApi, basename="cliente")
 urlpatterns = [
     path('',include(router.urls)),
     path('cl/<int:pk>/', cliente_detail_view, name= "cliente_api"),
+    path('boleta/<int:pk>/', boleta_detail_view, name= "boleta_api"),
 ]
