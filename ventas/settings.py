bd35542a26f3f82d988aa05245fc89cb93ab59b4
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-07 20:32:12 -0400

    Update settings.py

--- ventas/settings.py
+++ ventas/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['18.207.25.202','127.0.0.1','localhost','3.83.24.216']
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -52,13 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://18.207.25.202",
-    "18.207.25.202",
-    "http://127.0.0.1:8000/",
 ]
 
 ROOT_URLCONF = 'ventas.urls'
