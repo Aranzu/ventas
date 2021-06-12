0cddeb2d9b00e8609485b7743d38f24aa45d50fb
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-12 18:19:10 -0400

    Update views.py

--- boleta/views.py
+++ boleta/views.py
@@ -3,7 +3,10 @@ from rest_framework.response import Response
 from .serializers import ProductoSerializer, BoletaSerializer, ClienteSerializer
 from .models import Producto, Cliente, Boleta
 from rest_framework import filters
-
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # ViewSets define the view behavior.
 
 class ProductoApi(viewsets.ModelViewSet):
@@ -19,3 +22,23 @@ class ClienteApi(viewsets.ModelViewSet):
     permission_classes = ()
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+
+@api_view(['GET'])
+def cliente_detail_view(request,pk=None):
+	if request.method == 'GET':
+		cliente = Cliente.objects.filter(rut=pk).first()
+		if cliente == None:
+			return Response("No existe cliente.")
+		else:
+			cliente_serializer= ClienteSerializer(cliente)
+			return Response(cliente_serializer.data)
+
+@api_view(['GET'])
+def boleta_detail_view(request,pk=None):
+	if request.method == 'GET':
+		boleta = Boleta.objects.filter(num_boleta=pk).first()
+		if boleta == None:
+			return Response("No existe la boleta ingresada.")
+		else:
+			boleta_serializer= BoletaSerializer(boleta)
+			return Response(boleta_serializer.data)
