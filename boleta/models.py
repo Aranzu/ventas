1dffe3703eeda192a2ee2584a2f2d15e87b327bd
Aranzu <64505762+Aranzu@users.noreply.github.com>
2021-06-12 20:00:39 -0400

    Update models.py

--- boleta/models.py
+++ boleta/models.py
@@ -4,15 +4,12 @@ from django.utils import timezone
 # Create your models here.
 
 class Producto(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
+    id = models.IntegerField(primary_key=True)
     nombre_pro = models.CharField(max_length=100)
     precio = models.IntegerField()
 
-    def __str__(self):
-        return self.nombre_pro
-
 class Boleta(models.Model):
-    num_boleta = models.CharField(max_length=100, primary_key=True)
+    num_boleta = models.IntegerField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     total = models.IntegerField()
     productos = models.ManyToManyField('Producto', related_name='boletas', blank=True)
