995f027131b49dd70b3d4da7d3b03f183d4251f8
Aranzu <se.riquelme.a@gmail.com>
2021-06-13 13:05:22 -0400

    dejarlo como estaba

--- boleta/serializers.py
+++ boleta/serializers.py
@@ -7,11 +7,13 @@ class BoletaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Boleta
         fields = ('num_boleta','created_at','total','productos')
+        depth = 1
         
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = ('rut','nombre_cl','direccion','boletas')
+        depth = 2
 
 class ProductoSerializer(serializers.ModelSerializer):
     boletas = BoletaSerializer(many=True, read_only=True)
