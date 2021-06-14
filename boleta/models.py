ea84305c2c2f5b8b03378e96f6e0a37cfda3f5ec
Aranzu <se.riquelme.a@gmail.com>
2021-06-13 21:49:42 -0400

    tipodatos

--- boleta/models.py
+++ boleta/models.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 # Create your models here.
 
 class Producto(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
+    id = models.AutoField(primary_key=True)
     nombre_pro = models.CharField(max_length=100)
     precio = models.IntegerField()
 
 class Boleta(models.Model):
-    num_boleta = models.CharField(max_length=100, primary_key=True)
+    num_boleta = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     total = models.IntegerField()
     productos = models.ManyToManyField('Producto', related_name='boletas', blank=True)
