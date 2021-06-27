c81896fea56078b08af602859ec30ead00bdfc46
Aranzu <se.riquelme.a@gmail.com>
2021-06-27 11:23:16 -0400

    cambios modelo

--- ventas/settings.py
+++ ventas/settings.py
@@ -23,15 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')g+7b*0qlnfu6q7)er+5oirz+dac680wyq-)yn7ks#iefr)__r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.207.25.202','127.0.0.1','localhost','3.83.24.216']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +53,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://18.207.25.202",
+    "18.207.25.202",
+    "http://127.0.0.1:8000/",
+]
 
 ROOT_URLCONF = 'ventas.urls'
 
@@ -122,10 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
